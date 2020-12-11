9686b4ada535fe238deac024f499196c6eaf1478
IndonesiaJaya720 <72527640+IndonesiaJaya720@users.noreply.github.com>
2020-12-11 20:22:28 +0700

    Add files via upload

---
+++ WebdavIndo.sh
@@ -0,0 +1,334 @@
+#! / bin /bash
+
+red='\33[31;1m'
+yellow='\33[33;1m'
+blue='\33[0;36m'
+green='\33[0;32m'
+black='\33[30;1m'
+brown='\e[0;33m'
+darkgray='\e[1;30m'
+lightblue='\e[1;34m'
+lightcyan='\e[1;36m'
+white='\e[1;37m'
+nc='\e[0m'
+sleep 1
+clear
+toilet -f big -F gay [ Mr_R5B ]
+echo
+echo $red
+echo = " Hi Say Mr_R5B "
+echo = " 1 tools all yes "
+echo = " ddos attack "
+echo = " spam sms "
+echo = " phone spam "
+echo = " and others "
+echo = " So much from me, thanks "
+echo
+echo $blue
+echo = " I Will Tell You How To Install Everything Yes "
+echo = " We Hug Materials : "
+echo = " [ 1 ] Target Webdav "
+echo = " [ 2 ] IP "
+echo = " [ 3 ] ScriptSinyalWifi "
+echo = " [ 4 ] Exit "
+echo
+echo $black
+echo " =========================================="
+echo " =========================================="
+echo " ||    Atthur = Mr_R5B                   ||"
+echo " ||    WhatsApp = +628......             ||"
+echo " ||    Hobi = Website Hacker             ||"
+echo " ||    Message = IP = Port = Webset =    ||"
+echo " =========================================="
+echo " =========================================="
+echo
+echo $brown
+echo " { https://github.com/ } "
+echo " { http://defaceindonesia.6te.net/MR_R5B } "
+echo " { http://defaceindonesia.6te.net/ } "
+echo " { http://anonymousindonesia.6te.net/ } "
+echo
+echo $cyan
+echo [•] Installation Starts in 3 Seconds
+sleep 3
+echo [•] Install Data Yes First
+echo [•] Installation Starts in 3 Seconds
+sleep 3
+pkg update && pkg upgrade
+pkg install git
+pkg install libcurl
+pkg install bash -y
+pkg install figlet -y
+pkg install nano -y
+pkg install php -y
+pip2 install requests && pip2 install mechanize
+echo $yellow
+echo [•] Database Complete
+echo " [•] Let's start "
+echo [•] Start sleeping 3
+echo
+echo $blue
+echo " [%%%%%%] "
+sleep 1
+echo " [%%%%%%%%%%] "
+sleep 2
+echo " [%%%%%%%%%%%] "
+sleep 3
+echo " [%%%%%%%%%%%%] "
+sleep 4
+echo " [%%%%%%%%%%%%%] "
+sleep 5
+echo " [%%%%%%%%%%%%%%] "
+sleep 6
+echo " [%%%%%%%%%%%%%%%] "
+sleep 7
+echo " [%%%%%%%%%%%%%%%%] "
+sleep 8
+echo " [%%%%%%%%%%%%%%%%%] "
+sleep 9
+echo " [%%%%%%%%%%%%%%%%%%%] "
+sleep 10
+echo " [√] Thank you for waiting "
+echo " [√] Time We Read Success "
+echo " [√] "
+echo $yellow
+echo " { @~@~@~@~@~@~@~@~@~@~@~@~@~@~@~@~@~@ } "
+echo " { ~@~@~@~@~@~@~@~@~@~@~@~@~@~@~@~@~@  } "
+echo " {     [1] All DDOS                    } "
+echo " {     [2] All Phishing                } "
+echo " {     [3] All Spam-Sms                } "
+echo " {     [4] All Spam-Phone              } "
+echo " {     [5] All viruses                 } "
+echo " {     [6] All dork                    } "
+echo " {     [7] Exit                        } "
+echo " { @~@~@~@~@~@~@~@~@~@~@~@~@~@~@~@~@~@ } "
+echo " { ~@~@~@~@~@~@~@~@~@~@~@~@~@~@~@~@~@  } "
+read -p "masukan pilihan anda di sini :" Bro
+if [ $Bro = 1 ] || [ $Bro = 1 ]
+then
+      echo [1] == Torshammer
+      echo [2] == Wait for the installer to complete
+      echo [3] == Start sleeping 3
+apt update && apt upgrade -y
+pkg install git -y
+pkg install tor -y
+pkg install python2 -y
+git clone https://github.com/dotfighter/torshammer.git
+cd torshammer
+chmod 777 torshammer.py
+python2 torshammer.py
+      sleep 3
+      echo [4] == Installer Complete
+      echo [5] == The next step
+      echo
+      echo [1] == Hammer
+      echo [2] == Wait for the installer to complete
+      echo [3] == Start sleeping 3
+apt update && apt upgrade -y
+pkg install python -y
+pkg install git -y
+git clone http://github.com/cyweb/hammer
+cd hammer
+chmod 777 hammer.py
+python hammer.py
+      echo [4] == Installer Complete
+      echo [5] == The next step
+      echo
+      echo [1] == LITEDDOS
+      echo [2] == Wait for the installer to complete
+      echo [3] == Start sleeping 3
+apt update && apt upgrade -y
+pkg install git -y
+pkg install python2 -y
+git clone https://github.com/4L13199/LITEDDOS
+cd LITEDDOS
+chmod 777 liteDDOS.py
+python2 liteDDOS.py
+      echo [4] == Installer Complete
+      echo [5] == The next step
+      echo
+      sleep 3
+      echo [1] == XERXES
+      echo [2] == Wait for the installer to complete
+      echo [3] == Start sleeping 3
+apt update && apt upgrade -y
+pkg install git -y
+pkg install clang -y
+git clone https://github.com/zanyarjamal/xerxes
+cd xerxes
+clang xerxes.c -o xerxes
+chmod 777 xerxes
+./xerxes
+      echo [4] == Installer Complete
+      echo [5] == The next step
+      echo
+      sleep 3
+      echo [1] == Hulk
+      echo [2] == Wait for the installer to complete
+      echo [3] == Start sleeping 3
+apt update && apt upgrade -y
+pkg install git -y
+pkg install python2 -y
+git clone https://github.com/grafov/hulk.git
+cd hulk
+chmod +x hulk.py
+python2 hulk.py
+      echo [4] == Installer Complete
+      echo [5] == The next step
+      echo
+      sleep 3
+exit
+fi
+
+if [ $Bro = 2 ] || [ $Bro = 2 ]
+then
+      echo [1] == Weeman
+      echo [2] == Wait for the installer to complete
+      echo [3] == Start sleeping 3
+sleep 3
+pkg install python2 git
+pip2 install beutifulsoup
+git clone https://github.com/evait-security/weeman
+      echo [4] == Installer Complete
+      echo [5] == The next step
+      echo
+sleep 3
+      echo [1] == SocialPhish
+      echo [2] == Wait for the installer to complete
+      echo [3] == Start sleeping 3
+sleep 3
+git clone https://github.com/xHak9x/SocialPhish.git
+cd SocialPhish
+chmod +x socialphish.sh
+./socialphish.sh
+      echo [4] == Installer Complete
+      echo [5] == The next step
+      echo
+sleep 3
+      echo [1] == HiddenEye
+      echo [2] == Wait for the installer to complete
+      echo [3] == Start sleeping 3
+sleep 3
+pkg install git python php curl openssh grep
+pip install requests pgrep pyngrok
+https://github.com/DarkSecDevelopers/HiddenEye-Legacy
+cd HiddenEye-Legacy
+chmod 777 HiddenEye.py
+python2 HiddenEye.py
+      echo [4] == Installer Complete
+      echo [5] == The next step
+      echo
+sleep 3
+      echo [1] == PiPhish
+      echo [2] == Wait for the installer to complete
+      ephp SpamCall.phpcho [3] == Start sleeping 3
+sleep 3
+pkg install bash git python2 toilet figlet ruby
+gem install lolcat
+git clone https://github.com/ridwanirawan/PiPhish
+     echo [4] == Installer Complete
+     echo [5] == The next step
+     echo
+sleep 3
+     echo [1] == Shellphish
+     echo [2] == Wait for the installer to complete
+     echo [3] == Start sleeping 3
+sleep 3
+git clone https://github.com/thelinuxchoice/shellphish
+cd shellphish
+bash shellphish.sh
+     echo [4] == Installer Complete
+     echo [5] == The next step
+     echo
+sleep 3
+exit
+fi
+
+if [ $Bro = 3 ] || [ $Bro = 3 ]
+then
+      echo [1] == SpamSms
+      echo [2] == Wait for the installer to complete
+      echo [3] == Start sleeping 3
+sleep 3
+apt update && pkg upgrade
+apt install python
+apt install git
+git clone https://github.com/KANG-NEWBIE/SpamSms
+pip install requests mechanize bs4
+cd SpamSms
+python main.py
+exit
+fi
+
+if [ $Bro = 4 ] || [ $Bro = 4 ]
+then
+      echo [1] == SpamTelepon
+      echo [2] == Wait for the installer to complete
+      echo [3] == Start sleeping 3
+sleep 3
+pkg update && pkg upgrade
+pkg install php
+pkg install git
+git clone https://github.com/Aditya021/SpamCall
+cd SpamSms
+php SpamCall.php
+     echo [4] == Installer Complete
+     echo [5] == The next step
+sleep 3
+exit
+fi
+
+if [ $Bro = 5 ] || [ $Bro = 5 ]
+then
+     echo [1] == Virus Android
+     echo [2] == Wait for the installer to complete
+     echo [3] == Start sleeping 3
+sleep 3
+apt update
+apt upgrade -y
+pkg install python2 -y
+pkg install wget -y
+git clone https://github.com/storiku/virus
+cd vbug
+python2 vbug.py
+     echo [4] == Installer Complete
+     echo [5] == The next step
+sleep 3
+     echo [1] == Virus Android 2
+     echo [2] == Wait for the installer to complete
+     echo [3] == Start sleeping 3
+sleep 3
+pkg install update && pkg install upgrade
+pkg install python
+pkg install git
+git clone https://github.com/LOolzeC/Evil-create-framework
+cd Evil-create-framework
+sh install.sh
+     echo [4] == Installer Complete
+     echo [5] == The next step
+exit
+fi
+
+if [ $Bro = 6 ] || [ $Bro = 6 ]
+then
+     echo [1] == G-dork
+     echo [2] == Wait for the installer to complete
+     echo [3] == Start sleeping 3
+sleep 3
+pkg update && pkg upgrade
+pkg install git -y
+pkg install python -y
+pkg install python2 -y
+git clone https://github.com/obheda12/GitDorker
+cd GitDorker
+python3 GitDorker.py -h
+     echo [4] == Installer Complete
+     echo [5] == The next step
+     echo [6] == vuln ya
+exit
+fi
+
+if [ $Bro = 7 ] || [ $Bos = 7 ]
+then
+exit
+fi
